083e264635ec557aa8580b119b14ba4c352c437c
williamagaly <williamsantos25@gmail.com>
2024-08-25 04:56:12 -0300

    Estudo sobre palavras reservadas

---
+++ palavras-reservadas/src/PalavrasReservadas.java
@@ -0,0 +1,63 @@
+public class PalavrasReservadas {
+
+    
+    public static void main(String[] args) throws Exception {
+        //PALAVRAS RESERVADAS DE:
+          
+              //CONTROLE DE PACOTE
+              //import: - Importa pacotes ou classes para dentro do código
+              //package: - Especifica a que pacote todas as classes de um arquivo pertencem
+          
+              //MODIFICADORES DE ACESSO
+              //public: - A classe é acessível por qualquer outra classe
+              //private: - A classe só é acessível dentro da própria classe
+              //protected: - A classe é acessível por classes do mesmo pacote e por subclasses
+          
+              //PRIMITIVOS
+              //boolean: - Tipo de dado que armazena valores lógicos
+              //byte: - Tipo de dado que armazena valores inteiros de 8 bits
+              //char: - Tipo de dado que armazena valores de caracteres Unicode
+              //double: - Tipo de dado que armazena valores de ponto flutuante de 64 bits
+              //float: - Tipo de dado que armazena valores de ponto flutuante de 32 bits
+              //int: - Tipo de dado que armazena valores inteiros de 32 bits
+              //long: - Tipo de dado que armazena valores inteiros de 64 bits
+              //short: - Tipo de dado que armazena valores inteiros de 16 bits
+              //void: - Tipo de dado que não retorna nenhum valor
+          
+              //MODIFICADORES DE CLASSES, VARIÁVEIS E MÉTODOS 
+              //abstract: - A classe não pode ser instanciada, só pode ser herdada
+              //class: - Define uma classe
+              //extends: - Indica a superclasse que a subclasse está estendendo
+              //final: - impossibilita que uma classe seja extendida, que um método seja sobrescrito ou que uma variável seja reinitializada 
+              //implements: - Indica que uma classe está implementando uma interface
+              //interface: - Define uma interface
+              //native: - Indica que um método é implementado em código nativo
+              //new: - Aloca memória para um novo objeto
+              //static: - Indica que um método ou variável pertence à classe, e não a instâncias da classe
+              //strictfp: - Restringe a precisão de ponto flutuante
+              //synchronized: - Indica que um método só pode ser acessado por uma thread por vez
+              //transient: - Indica que uma variável não deve ser serializada
+              //volatile: - Indica que uma variável pode ser alterada por diferentes threads
+              
+              //CONTROLE DE FLUXO DENTRO DE UM BLOCO DE CÓDIGO
+              //break: - Sai de um loop ou switch
+              //case: - Define um valor para um switch
+              //continue: - Pula para a próxima iteração de um loop
+              //default: - Define o bloco de código a ser executado em um switch
+              //do: - Inicia um loop do-while
+              //else: - Indica o bloco de código a ser executado se a condição de um if for falsa
+              //for: - Inicia um loop for
+              //if: - Indica o bloco de código a ser executado se a condição for verdadeira
+              
+              //TRATAMENTO DE ERROS
+              //catch: - Captura exceções geradas por um bloco de código
+              //finally: - Indica um bloco de código que será executado após um bloco try-catch
+              //throw: - Lança uma exceção
+              //throws: - Indica que um método pode lançar exceções
+              //try: - Inicia um bloco de código que será testado quanto a exceções
+          
+              //VARIAVEIS DE REFERÊNCIA
+              //super: - Referencia a superclasse imediata de um objeto
+              //this: - Referencia o objeto atual
+    }
+}
